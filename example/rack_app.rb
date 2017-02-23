5def5371e230be72961e419af931bd8958c1d0b4
Shota Iguchi <shota-iguchi@cookpad.com>
2017-02-24 02:45:33 +0900

    Update examples

--- example/rack_app.rb
+++ example/rack_app.rb
@@ -5,15 +5,15 @@ class Application
   def call(env)
     status = 200
     headers = { 'Content-Type' => 'text/html' }
-    html = <<-EOS
-<html>
-  <head>
-    <title>sample app</title>
-  </head>
-  <body>
-    <p>app body</p>
-  </body>
-</html>
+    html = <<~EOS
+        <html>
+          <head>
+            <title>sample app</title>
+          </head>
+          <body>
+            <p>app body</p>
+          </body>
+        </html>
       EOS
 
     [status, headers, [html]]
