cb5a975322426aad3dc91138bbf1f954292664da
Shota Iguchi <shota-iguchi@cookpad.com>
2019-03-10 22:08:59 +0900

    Use chromedriver on CI environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ Capybara.register_driver :selenium do |app|
     browser: :chrome,
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
       chrome_options: {
-        args: %w(headless disable-gpu window-size=1920,1080),
+        args: %w(no-sandbox headless disable-gpu window-size=1920,1080),
       },
     )
   )
